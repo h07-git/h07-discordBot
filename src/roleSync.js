bc3531e4450dc2efd7306dfaac5883d1d5f2fa00
Harry <harryjwestworth@icloud.com>
2023-03-16 19:03:42 +0000

    Update roleSync.js

--- src/roleSync.js
+++ src/roleSync.js
@@ -1,23 +1,45 @@
-function syncRoles(client, config, member) {
-  for (const server of config.servers) {
-    if (server.id === member.guild.id) {
-      for (const syncedServer of config.servers) {
-        if (syncedServer.id !== server.id) {
-          const syncedMember = client.guilds.cache.get(syncedServer.id).members.cache.get(member.id);
-          if (syncedMember) {
-            syncedMember.roles.set(member.roles.cache)
-              .catch(error => {
-                console.error(`Failed to sync roles for ${syncedMember.user.tag} in ${syncedServer.name}:`, error);
-                errorLogger.logError(client, config, `Failed to sync roles for ${syncedMember.user.tag} in ${syncedServer.name}: ${error.message}`);
-              });
-          }
-        }
-      }
-      break;
-    }
-  }
-}
+const config = require('../config.json');
 
 module.exports = {
-  syncRoles
+  syncRoles: async (guild) => {
+    // Loop through all the predefined role names in the config
+    for (const roleName of config.roleNames) {
+      // Find the role with the same name in the current guild
+      const role = guild.roles.cache.find(r => r.name === roleName);
+      if (!role) {
+        console.log(`Could not find role ${roleName} in ${guild.name}. Skipping role sync.`);
+        continue;
+      }
+
+      // Loop through all the other guilds in the config and sync the role
+      for (const guildId of config.guildIds) {
+        if (guild.id === guildId) {
+          continue;
+        }
+        const otherGuild = await guild.client.guilds.fetch(guildId);
+        if (!otherGuild) {
+          console.log(`Could not find guild with ID ${guildId}. Skipping role sync.`);
+          continue;
+        }
+
+        // Find the role with the same name in the other guild
+        const otherRole = otherGuild.roles.cache.find(r => r.name === roleName);
+        if (!otherRole) {
+          console.log(`Could not find role ${roleName} in ${otherGuild.name}. Skipping role sync.`);
+          continue;
+        }
+
+        // Add or remove the role from the member
+        otherRole.members.forEach((member) => {
+          if (member.roles.cache.has(otherRole.id) && !member.roles.cache.has(role.id)) {
+            member.roles.remove(otherRole);
+            console.log(`Removed role ${roleName} from ${member.user.username} in ${otherGuild.name}.`);
+          } else if (!member.roles.cache.has(otherRole.id) && member.roles.cache.has(role.id)) {
+            member.roles.add(otherRole);
+            console.log(`Added role ${roleName} to ${member.user.username} in ${otherGuild.name}.`);
+          }
+        });
+      }
+    }
+  }
 };
